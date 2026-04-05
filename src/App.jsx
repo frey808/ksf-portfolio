a3944488cc9fa96c24e2a1995eab7a891bbb98b1
frey808 <kaifrazier413@gmail.com>
2026-04-04 21:35:53 -0400

    fixed deploy

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { NotFound } from "./pages/NotFound"
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <BrowserRouter basename="/ksf-portfolio">
         <Routes>
           <Route index element={<Home />} />
           <Route path="*" element={<NotFound />} />
