7397e1752dbe5b3603ca4f2b7bf6d678b16275cc
frey808 <kaifrazier413@gmail.com>
2026-04-12 20:48:01 -0400

    finishing touches

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,6 +6,7 @@ import { AboutSection } from "@/components/AboutSection"
 // import { SkillsSection } from "@/components/SkillsSection"
 import { ProjectsSection } from "@/components/ProjectsSection"
 import { ContactSection } from "@/components/ContactSection"
+import { Footer } from "../components/Footer"
 
 export const Home = () => {
   return (
@@ -23,12 +24,12 @@ export const Home = () => {
       <main>
         <HeroSection />
         <AboutSection />
-        {/* <SkillsSection /> */}
         <ProjectsSection />
         <ContactSection />
       </main>
 
       {/* Footer */}
+      <Footer />
     </div>
   )
 }
